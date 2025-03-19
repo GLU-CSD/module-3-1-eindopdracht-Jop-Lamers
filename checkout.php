bed85d29d935120306f7b5f5c0df3f565e2d32cc
Jop-Lamers <Jop14.lamers@gmail.com>
2025-03-19 12:07:03 +0100

    klaar voor gebruik

--- checkout.php
+++ checkout.php
@@ -150,6 +150,31 @@
 
     </section>
   </form>
+  <footer>
+    <div class="footer-container">
+      <div class="information">
+        <h3 id="info-font">-INFORMATION-</h3>
+        <h4><a href="#">About Us</a></h4>
+        <h4><a href="#">Privacy Policy</a></h4>
+        <h4><a href="#">Terms & Conditions</a></h4>
+        <h4><a href="#">FAQ</a></h4>
+      </div>
+      <div class="socials">
+        <h3 id="social-font">-SOCIALS-</h3>
+        <h4><a href="#">Instagram</a></h4>
+        <h4><a href="#">Facebook</a></h4>
+        <h4><a href="#">Twitter</a></h4>
+        <h4><a href="#">TikTok</a></h4>
+      </div>
+      <div class="contact">
+        <h3 id="contact-font">-CONTACT-</h3>
+        <h4>Email: <a href="mailto:info@dstore.com">info@dstore.com</a></h4>
+        <h4>Phone: <a href="tel:+1234567890">+31 000 000 00</a></h4>
+        <h4>Address: Kon. Wilhelminalaan 7, 3527 LA Utrecht </h4>
+      </div>
+    </div>
+    <p id="copyrighttxt"> copyright&copy; -2025- D's Store @D's_store.</p>
+  </footer>
 </body>
 
 </html>
