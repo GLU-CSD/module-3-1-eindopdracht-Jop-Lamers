4a07adba7c67a57564805ca2b204d6d0e0e94d70
Jop-Lamers <Jop14.lamers@gmail.com>
2025-03-19 14:41:01 +0100

    klaar

--- index.php
+++ index.php
@@ -26,9 +26,6 @@
   <meta name="theme-color" content="#fafafa" />
 </head>
 
-<head>
-</head>
-
 <body>
 
   <?php include 'includes/header.php'; ?>
@@ -107,267 +104,141 @@
 
     <div id="Shoes" class="product-container">
 
-      <div class="shoe-container">
+      <?php
+      $array_titels = [
+        "Asics gel nyc cream beige pink",
+        "Asics gel nyc cream kale",
+        "Asics gel nyc panelled",
+        "Asics gel-NYC smoke grey",
+        "x Gnarhunters SB Dunk Low sneakers",
+        "x Grateful Dead SB Dunk Low Orange",
+        "SB Dunk Low Mummy sneakers",
+        "Air Jordan 4 Retro Taupe Haze",
+        "Air Jordan 4 Retro SP Union Off Noir",
+        "x Travis Scott Air Jordan 1 Low Golf",
+        "Air Jordan 4 Retro Black Cat 2020",
+        "Air Jordan 4 Retro Motorsports",
+        "x Air Jordan 1 High OG Dusted Clay",
+        "Air Jordan 1 Retro High OG Chicago",
+        "Air Jordan 4 Retro Pure Money",
+        "Air Jordan 4 Retro Levis NRG white denim",
+        "Air Jordan 4 SB Pine Green sneakers",
+        "Air Jordan 1 Retro High Off-White-UNC",
+        "Off-White The 10: Air Jordan 1 Chicago",
+        "Kaws Air Jordan 4 Retro sneakers"
+      ];
+      $array_prijzen = [
+        239.99,
+        979.99,
+        319.99,
+        279.99,
+        699.99,
+        9829.99,
+        779.99,
+        728.99,
+        1375.99,
+        1129.99,
+        2149.99,
+        959.99,
+        289.99,
+        2129.99,
+        2878.99,
+        1868.99,
+        839.99,
+        3429.99,
+        14894.99,
+        736.99
+      ];
+      $array_images = [
+        "assets/img/pink gel nyc pink.webp",
+        "assets/img/cream kale nyc.webp",
+        "assets/img/pannelled.webp",
+        "assets/img/smoke grey.webp",
+        "assets/img/gnarhunters.webp",
+        "assets/img/grateful dreads.webp",
+        "assets/img/mummy dunks.webp",
+        "assets/img/taupe haze.webp",
+        "assets/img/unions.webp",
+        "assets/img/travis golfs.webp",
+        "assets/img/black cats.webp",
+        "assets/img/motorsports.webp",
+        "assets/img/dusted clay.webp",
+        "assets/img/chicago.webp",
+        "assets/img/pure moneys.webp",
+        "assets/img/levi.webp",
+        "assets/img/Air Jordan 4 SB Pine Green sneakers.webp",
+        "assets/img/unc jordan off white.webp",
+        "assets/img/chicago off white.webp",
+        "assets/img/kaws.webp"
+      ];
+      $array_brands = [
+        "Asics",
+        "Asics",
+        "Asics",
+        "Asics",
+        "Nike",
+        "Nike",
+        "Nike",
+        "Nike Air Jordan",
+        "Nike Air Jordan",
+        "Nike Air Jordan",
+        "Nike Air Jordan",
+        "Nike",
+        "Nike",
+        "Nike Air Jordan",
+        "Nike Air Jordan",
+        "Nike Air Jordan",
+        "Nike sb",
+        "Nike Air Jordan",
+        "Nike Air Jordan",
+        "Nike Air Jordan"
+      ];
+      $array_sizes = [
+        "36 - 38 - 42 - 43 - 45",
+        "36 - 38 - 41 - 42 - 43 - 44",
+        "39 - 40 - 44",
+        "34 - 38 - 39 - 40 - 42 - 44",
+        "40 - 44 - 47",
+        "42 - 46",
+        "36 - 40 - 47",
+        "44 - 45 - 48 - 49",
+        "39 - 40 - 41 - 44 - 46",
+        "44 - 45 - 49",
+        "36 - 38 - 39 - 41 - 45",
+        "34 - 35 - 40 - 44",
+        "40 - 41 - 50",
+        "43 - 46 - 49",
+        "36 - 46 - 47 - 49",
+        "44 - 45 - 48 - 49",
+        "40 - 44 - 47",
+        "42 - 46",
+        "40 - 47",
+        "41 - 45 - 47 - 49"
+      ];
+
+      for ($a = 0; $a < count($array_titels); $a++) {
+
+      ?>
+
         <div class="shoe">
-          <h3>Asics gel nyc cream beige pink</h3>
+          <h3><?php echo ($array_titels[$a]); ?></h3>
           <img
-            src="assets/img/pink gel nyc pink.webp"
+            src="<?php echo ($array_images[$a]) ?>"
             alt="Asics gel nyc cream mineral beige pink" />
-          <p class="prijs">Price: -$239,99-</p>
+          <p class="prijs">Price: -<?php echo ($array_prijzen[$a]); ?>-</p>
           <div class="info">
-            <p><strong>Brand name:</strong> Asics</p>
-            <p><strong>Sizes Available:</strong> 36 - 38 - 42 - 43 - 45</p>
+            <p><strong>Brand name:</strong> <?php echo ($array_brands[$a]); ?></p>
+            <p><strong>Sizes Available:</strong> <?php echo ($array_sizes[$a]); ?></p>
             <button class="add-to-cart" onclick="addToCart('Asics gel nyc cream beige pink', 239.99)">Add to cart</button>
           </div>
         </div>
 
-        <div class="shoe">
-          <h3>Asics gel nyc cream kale</h3>
-          <img
-            src="assets/img/cream kale nyc.webp"
-            alt="Asics gel nyc cream kale" />
-          <p class="prijs">Price: -$979,99-</p>
-          <div class="info">
-            <p><strong>Brand name:</strong> Asics</p>
-            <p>
-              <strong>Sizes Available:</strong> 36 - 38 - 41 - 42 - 43 - 44
-            </p>
-            <button class="add-to-cart" onclick="addToCart('Asics gel nyc cream kale', 979.99)">Add to cart</button>
-          </div>
-        </div>
-        <div class="shoe">
-          <h3>Asics gel nyc panelled</h3>
-          <img src="assets/img/pannelled.webp" alt="Asics gel nyc panelled" />
-          <p class="prijs">Price: -$319,99-</p>
-          <div class="info">
-            <p><strong>Brand name:</strong> Asics</p>
-            <p><strong>Sizes Available:</strong> 39 - 40 - 44</p>
-            <button class="add-to-cart" onclick="addToCart('Asics gel nyc panelled', 319.99)">Add to cart</button>
-          </div>
-        </div>
+      <?php
+      }
+      ?>
 
-        <div class="shoe">
-          <h3>Asics gel-NYC smoke grey</h3>
-          <img
-            src="assets/img/smoke grey.webp"
-            alt="Asics gel-NYC smoke grey" />
-          <p class="prijs">Price: -$279,99-</p>
-          <div class="info">
-            <p class="prijs"><strong>Brand name:</strong> Asics</p>
-            <p>
-              <strong>Sizes Available:</strong> 34 - 38 - 39 - 40 - 42 - 44
-            </p>
-            <button class="add-to-cart" onclick="addToCart('Asics gel-NYC smoke grey', 279.99)">Add to cart</button>
-          </div>
-        </div>
-
-        <div class="shoe">
-          <h3>x Gnarhunters SB Dunk Low sneakers</h3>
-          <img
-            src="assets/img/gnarhunters.webp"
-            alt="x Gnarhunters SB Dunk Low sneakers" />
-          <p class="prijs">Price: -$699,99-</p>
-          <div class="info">
-            <p><strong>Brand name:</strong> Nike</p>
-            <p><strong>Sizes Available:</strong> 40 - 44 - 47</p>
-            <button class="add-to-cart" onclick="addToCart('x Gnarhunters SB Dunk Low sneakers', 699.99)">Add to cart</button>
-          </div>
-        </div>
-
-        <div class="shoe">
-          <h3>x Grateful Dead SB Dunk Low "Orange"</h3>
-          <img
-            src="assets/img/grateful dreads.webp"
-            alt="x Grateful Dead SB Dunk Low Orange" />
-          <p class="prijs">Price: -$9829,99-</p>
-          <div class="info">
-            <p><strong>Brand name:</strong> Nike</p>
-            <p><strong>Sizes Available:</strong> 42 - 46</p>
-            <button class="add-to-cart" onclick="addToCart('x Grateful Dead SB Dunk Low Orange', 9829.99)">Add to cart</button>
-          </div>
-        </div>
-
-        <div class="shoe">
-          <h3>SB Dunk Low "Mummy" sneakers</h3>
-          <img
-            src="assets/img/mummy dunks.webp"
-            alt="SB Dunk Low Mummy sneakers" />
-          <p class="prijs">Price: -$779,99-</p>
-          <div class="info">
-            <p><strong>Brand name:</strong> Nike</p>
-            <p><strong>Sizes Available:</strong> 36 - 40 - 47</p>
-            <button class="add-to-cart" onclick="addToCart('SB Dunk Low Mummy sneakers', 779.99)">Add to cart</button>
-          </div>
-        </div>
-
-        <div class="shoe">
-          <h3>Air Jordan 4 Retro "Taupe Haze"</h3>
-          <img
-            src="assets/img/taupe haze.webp"
-            alt="Air Jordan 4 Retro Taupe Haze" />
-          <p class="prijs">Price: -$728,99-</p>
-          <div class="info">
-            <p><strong>Brand name:</strong> Nike Air Jordan</p>
-            <p><strong>Sizes Available:</strong> 44 - 45 - 48 - 49</p>
-            <button class="add-to-cart" onclick="addToCart('Air Jordan 4 Retro Taupe Haze', 728.99)">Add to cart</button>
-          </div>
-        </div>
-
-        <div class="shoe">
-          <h3>Air Jordan 4 Retro SP "Union Off Noir"</h3>
-          <img
-            src="assets/img/unions.webp"
-            alt="Air Jordan 4 Retro SP Union Off Noir" />
-          <p class="prijs">Price: -$1375,99-</p>
-          <div class="info">
-            <p><strong>Brand name:</strong> Nike Air Jordan</p>
-            <p><strong>Sizes Available:</strong> 39 - 40 - 41 - 44 - 46</p>
-            <button class="add-to-cart" onclick="addToCart('Air Jordan 4 Retro SP Union Off Noir', 1375.99)">Add to cart</button>
-          </div>
-        </div>
-
-        <div class="shoe">
-          <h3>x Travis Scott Air Jordan 1 Low Golf</h3>
-          <img
-            src="assets/img/travis golfs.webp"
-            alt="x Travis Scott Air Jordan 1 Low Golf" />
-          <p class="prijs">Price: -$1129.99-</p>
-          <div class="info">
-            <p><strong>Brand name:</strong> Nike Air Jordan</p>
-            <p><strong>Sizes Available:</strong> 44 - 45 - 49</p>
-            <button class="add-to-cart" onclick="addToCart('x Travis Scott Air Jordan 1 Low Golf', 1129.99)">Add to cart</button>
-          </div>
-        </div>
-
-        <div class="shoe">
-          <h3>Air Jordan 4 Retro Black Cat 2020</h3>
-          <img
-            src="assets/img/black cats.webp"
-            alt="Air Jordan 4 Retro Black Cat 2020" />
-          <p class="prijs">Price: -$2149,99-</p>
-          <div class="info">
-            <p><strong>Brand name:</strong> Nike Air Jordan</p>
-            <p><strong>Sizes Available:</strong> 36 - 38 - 39 - 41 - 45</p>
-            <button class="add-to-cart" onclick="addToCart('Air Jordan 4 Retro Black Cat 2020', 2149.99)">Add to cart</button>
-          </div>
-        </div>
-
-        <div class="shoe">
-          <h3>Air Jordan 4 Retro "Motorsports"</h3>
-          <img
-            src="assets/img/motorsports.webp"
-            alt="Air Jordan 4 Retro Motorsports" />
-          <p class="prijs">Price: -$959,99-</p>
-          <div class="info">
-            <p><strong>Brand name:</strong> Nike Air Jordan</p>
-            <p><strong>Sizes Available:</strong> 34 - 35 - 40 - 44</p>
-            <button class="add-to-cart" onclick="addToCart('Air Jordan 4 Retro Motorsports', 959.99)">Add to cart</button>
-          </div>
-        </div>
-        <div class="shoe">
-          <h3>x Air Jordan 1 High OG "Dusted Clay"</h3>
-          <img
-            src="assets/img/dusted clay.webp"
-            alt="Air Jordan 1 High OG Dusted Clay" />
-          <p class="prijs">Price: -$289,99-</p>
-          <div class="info">
-            <p><strong>Brand name:</strong> Nike</p>
-            <p><strong>Sizes Available:</strong> 40 - 41 - 50</p>
-            <button class="add-to-cart" onclick="addToCart('x Air Jordan 1 High OG Dusted Clay', 289.99)">Add to cart</button>
-          </div>
-        </div>
-
-        <div class="shoe">
-          <h3>Air Jordan 1 Retro High OG "Chicago"</h3>
-          <img
-            src="assets/img/chicago.webp"
-            alt="Air Jordan 1 Retro High OG Chicago" />
-          <p class="prijs">Price: -$2129,99-</p>
-          <div class="info">
-            <p><strong>Brand name:</strong> Nike Air Jordan</p>
-            <p><strong>Sizes Available:</strong> 43 - 46 - 49</p>
-            <button class="add-to-cart" onclick="addToCart('Air Jordan 1 Retro High OG Chicago', 2129.99)">Add to cart</button>
-          </div>
-        </div>
-
-        <div class="shoe">
-          <h3>Air Jordan 4 Retro "Pure Money"</h3>
-          <img
-            src="assets/img/pure moneys.webp"
-            alt="Air Jordan 4 Retro Pure Money" />
-          <p class="prijs">Price: -$2878,99-</p>
-          <div class="info">
-            <p><strong>Brand name:</strong> Nike Air Jordan</p>
-            <p><strong>Sizes Available:</strong> 36 - 46 - 47 - 49</p>
-            <button class="add-to-cart" onclick="addToCart('Air Jordan 4 Retro Pure Money', 2878.99)">Add to cart</button>
-          </div>
-        </div>
-
-        <div class="shoe">
-          <h3>Air Jordan 4 Retro Levis NRG white denim</h3>
-          <img
-            src="assets/img/levi.webp"
-            alt="Air Jordan 4 Retro Levis NRG white denim" />
-          <p class="prijs">Price: -$1868,99-</p>
-          <div class="info">
-            <p><strong>Brand name:</strong> Nike Air Jordan</p>
-            <p><strong>Sizes Available:</strong> 44 - 45 - 48 - 49</p>
-            <button class="add-to-cart" onclick="addToCart('Air Jordan 4 Retro Levis NRG white denim', 1868.99)">Add to cart</button>
-          </div>
-        </div>
-        <div class="shoe">
-          <h3>Air Jordan 4 SB Pine Green sneakers</h3>
-          <img
-            src="assets/img/Air Jordan 4 SB Pine Green sneakers.webp"
-            alt="Air Jordan 4 SB Pine Green sneakers" />
-          <p class="prijs">Price: -$839,99-</p>
-          <div class="info">
-            <p><strong>Brand name:</strong> Nike sb</p>
-            <p><strong>Sizes Available:</strong> 40 - 44 - 47</p>
-            <button class="add-to-cart" onclick="addToCart('Air Jordan 4 SB Pine Green sneakers', 839.99)">Add to cart</button>
-          </div>
-        </div>
-
-        <div class="shoe">
-          <h3>Air Jordan 1 Retro High "Off-White-UNC"</h3>
-          <img
-            src="assets/img/unc jordan off white.webp"
-            alt="Air Jordan 1 Retro High Off-White-UNC" />
-          <p class="prijs">Price: -$3429,99-</p>
-          <div class="info">
-            <p><strong>Brand name:</strong> Nike Air Jordan</p>
-            <p><strong>Sizes Available:</strong> 42 - 46</p>
-            <button class="add-to-cart" onclick="addToCart('Air Jordan 1 Retro High Off-White-UNC', 3429.99)">Add to cart</button>
-          </div>
-        </div>
-
-        <div class="shoe">
-          <h3>Off-White The 10: Air Jordan 1 "Chicago"</h3>
-          <img
-            src="assets/img/chicago off white.webp"
-            alt="Off-White The 10: Air Jordan 1 Chicago sneakers" />
-          <p class="prijs">Price: -$14894,99-</p>
-          <div class="info">
-            <p><strong>Brand name:</strong> Nike Air Jordan</p>
-            <p><strong>Sizes Available:</strong> 40 - 47</p>
-            <button class="add-to-cart" onclick="addToCart('Off-White The 10: Air Jordan 1 Chicago', 14894.99)">Add to cart</button>
-          </div>
-        </div>
-
-        <div class="shoe">
-          <h3>Kaws Air Jordan 4 Retro sneakers</h3>
-          <img
-            src="assets/img/kaws.webp"
-            alt=" Kaws Air Jordan 4 Retro sneakers" />
-          <p class="prijs">Price: -$736,99-</p>
-          <div class="info">
-            <p><strong>Brand name:</strong> Nike Air Jordan</p>
-            <p><strong>Sizes Available:</strong> 41 - 45 - 47 - 49</p>
-            <button class="add-to-cart" onclick="addToCart('Kaws Air Jordan 4 Retro sneakers', 736.99)">Add to cart</button>
-          </div>
-        </div>
-      </div>
     </div>
+  </div>
 
   </div>
 
