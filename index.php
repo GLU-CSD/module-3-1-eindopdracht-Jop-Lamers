a878cf563a8f1a9b7cf0cbc8054e68f2a138c2c2
Jop-Lamers <Jop14.lamers@gmail.com>
2025-04-01 11:37:30 +0200

    sidenav include

--- index.php
+++ index.php
@@ -34,74 +34,9 @@
 
   <div id="main">
 
-    <div class="sidenav">
-      <h1 class="filter-text">-Filters-</h1>
-
-      <p id="font-adjust">-Category's-</p>
-      <label class="container"> Shoes
-        <input type="checkbox">
-        <span class="checkmark"></span>
-      </label>
-      <label class="container"> Pants
-        <input type="checkbox">
-        <span class="checkmark"></span>
-      </label>
-      <label class="container"> Jackets
-        <input type="checkbox">
-        <span class="checkmark"></span>
-      </label>
-      <label class="container"> Hoodies
-        <input type="checkbox">
-        <span class="checkmark"></span>
-      </label>
-      <label class="container"> Belts
-        <input type="checkbox">
-        <span class="checkmark"></span>
-      </label>
-      <label class="container"> Sunglasses
-        <input type="checkbox">
-        <span class="checkmark"></span>
-      </label>
-      <label class="container"> Outerwear
-        <input type="checkbox">
-        <span class="checkmark"></span>
-      </label>
-
-      <p id="font-adjust">- Popular Brands-</p>
-      </label>
-      <label class="container"> Supreme
-        <input type="checkbox">
-        <span class="checkmark"></span>
-      </label>
-      <label class="container"> Nike Air Jordan
-        <input type="checkbox">
-        <span class="checkmark"></span>
-      </label>
-      <label class="container"> Nike SB
-        <input type="checkbox">
-        <span class="checkmark"></span>
-      </label>
-      <label class="container"> Asics
-        <input type="checkbox">
-        <span class="checkmark"></span>
-      </label>
-      <label class="container"> Diesel
-        <input type="checkbox">
-        <span class="checkmark"></span>
-      </label>
-      <label class="container"> Gucci
-        <input type="checkbox">
-        <span class="checkmark"></span>
-      </label>
-
-      <p id="font-adjust">-Price adjust-</p>
-
-      <div class="slidecontainer">
-        <input type="range" min="1" max="15000" value="" class="slider" id="myRange">
-        <p id=price-amount>Amount: <span id="demo"></span></p>
-      </div>
-    </div>
-
+    <?php
+    include 'includes/sidenav.php';
+    ?>
     <div id="Shoes" class="product-container">
 
       <?php
