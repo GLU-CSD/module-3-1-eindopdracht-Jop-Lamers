4e48b546605747f4e4c7595e81e1961333615f9c
Jop-Lamers <Jop14.lamers@gmail.com>
2025-04-08 10:04:37 +0200

    tab indexxes

--- checkout.php
+++ checkout.php
@@ -44,46 +44,48 @@
   <form action="result.php" method="get" onsubmit="return validateForm()">
     <section class="form">
       <div class="fill-in-forms">
+        <h2>Fill in your details</h2>
+        
         <!-- Geslacht selectie -->
         <label>Preposition:</label>
-        <input type="radio" id="male" name="Gender" value="male" checked />
+        <input type="radio" id="male" name="Gender" value="male" tabindex="1" checked />
         <label for="male"> Dhr. </label>
-        <input type="radio" id="female" name="Gender" value="female" />
+        <input type="radio" id="female" name="Gender" value="female" tabindex="2" />
         <label for="female"> Mvr. </label>
-        <input type="radio" id="other" name="Gender" value="other" />
+        <input type="radio" id="other" name="Gender" value="other" tabindex="3" />
         <label for="other"> Other </label><br /><br />
 
         <!-- Voornaam invoerveld -->
         <label>First name:</label>
-        <input type="text" id="firstName" name="firstname" /><br /><br />
+        <input type="text" id="firstName" name="firstname" tabindex="4" /><br /><br />
 
         <!-- Achternaam invoerveld -->
         <label>Last name:</label>
-        <input type="text" id="lastName" name="lastname" /><br /><br />
+        <input type="text" id="lastName" name="lastname" tabindex="5" /><br /><br />
 
         <!-- Straatnaam invoerveld -->
         <label>Street:</label>
-        <input type="text" id="street" name="street" /><br /><br />
+        <input type="text" id="street" name="street" tabindex="6" /><br /><br />
 
         <!-- Huisnummer invoerveld -->
         <label>Housenumber:</label>
-        <input type="text" id="houseNumber" name="housenumber" /><br /><br />
+        <input type="text" id="houseNumber" name="housenumber" tabindex="7" /><br /><br />
 
         <!-- Adres invoerveld -->
         <label>Address:</label>
-        <input type="text" id="address" name="address" /><br /><br />
+        <input type="text" id="address" name="address" tabindex="8" /><br /><br />
 
         <!-- E-mailadres invoerveld -->
         <label>E-mailaddress:</label>
-        <input type="email" id="email" name="e-mail_address" /><br /><br />
+        <input type="email" id="email" name="e-mail_address" tabindex="9" /><br /><br />
 
         <!-- Telefoonnummer invoerveld -->
         <label>Telephonenumber:</label>
-        <input type="tel" id="phoneNumber" name="telephonenumber" /><br /><br />
+        <input type="tel" id="phoneNumber" name="telephonenumber" tabindex="10" /><br /><br />
 
         <!-- Land selectie dropdown -->
         <label>Country:</label>
-        <select id="country" name="country">
+        <select id="country" name="country" tabindex="11">
           <option value="Netherlands">Netherlands</option>
           <option value="Belgium">Belgium</option>
           <option value="Germany">Germany</option>
@@ -92,15 +94,15 @@
 
         <!-- Geboortedatum invoerveld -->
         <label>Date of birth:</label>
-        <input type="date" id="dob" name="date_of_birth" /><br /><br />
+        <input type="date" id="dob" name="date_of_birth" tabindex="12" /><br /><br />
 
         <!-- Checkbox voor acceptatie van de voorwaarden -->
-        <input type="checkbox" id="policy" name="policy" />
+        <input type="checkbox" id="policy" name="policy" tabindex="13" />
         <label for="policy" href="policy.php">I accept the policy's</label><br /><br />
       </div>
 
       <!-- Knop om het formulier te verzenden -->
-      <input class="knop" type="submit">Finish order</input>
+      <input class="knop" type="submit" tabindex="14">Finish order</input>
     </section>
   </form>
 
